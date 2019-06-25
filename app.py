d734fcb34b07631965006a83dca38317cbd49b35
Samuel Lekatompessy <45459712+lekatompessy@users.noreply.github.com>
2019-06-25 21:25:58 +0700

    simplify and adding score

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ class bcolors:
     G = '\033[32m'
 
 arr = []
+score = 0
+hira_quiz = 0
 kana = data['kana']
 for x in kana:
     arr.append(x)
@@ -20,13 +22,19 @@ random.shuffle(arr)
 for hira in arr:
     input_kana = input(f"Please tell me what is this? {hira['hira']}\nYou Answer: ")
     romaji = hira['romaji']
+    hira_quiz += 1
 
     romaji = romaji if isinstance(romaji, list) else [romaji]
     romaji = '/'.join(romaji) if romaji[0] else romaji
 
     if input_kana == 'exit':
+        print('=' * 30)
+        print(f'- Your total score is {bcolors.G}{score}{bcolors.W} from {hira_quiz - 1} hiragana')
         exit()
     elif input_kana in romaji:
         print(bcolors.G + 'You are right!' + bcolors.W)
+        score += 1
     else:
         print(bcolors.R + f'You are wrong, the right is: {romaji}' + bcolors.W)
+
+print(f'Your total score is {bcolors.G}{score}{bcolors.W} from {hira_quiz} hiragana')
